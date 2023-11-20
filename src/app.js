264b3239a47d6ba84082fca871fb9840ec51a865
Crystal <crystal120599@gmail.com>
2023-11-20 10:27:18 -0800

    Create route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 //TODO: Create your GET Request Route Below:
 app.get("/restaurants", async(req, res) =>{
     let restaurants = await Restaurant.findAll();
@@ -13,6 +16,12 @@ app.get(`/restaurants/:id`, async(req,res) =>{
     let id = req.params.id
     let restaurant = await Restaurant.findByPk(id)
     res.json(restaurant)
+})
+
+app.post("/restaurants", async(req,res) =>{
+    let newRes = await Restaurant.create(req.body);
+    res.json(newRes)
+
 })
 
 
