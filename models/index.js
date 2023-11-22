3d47b7dbd5121085df0cd5337000ec81f408e96d
Crystal <crystal120599@gmail.com>
2023-11-22 13:58:09 -0800

    association and seeds

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 const Restaurant = require('./Restaurant')
+const Menu = require('./Menu');
+const Item = require('./Item')
 
+Restaurant.hasMany(Menu)
+Menu.belongsTo(Restaurant);
 
-module.exports = Restaurant;
+Menu.belongsToMany(Item, {through: 'MenuItem'});
+Item.belongsToMany(Menu,{through: 'MenuItem'})
+module.exports = {Restaurant, Menu, Item};
